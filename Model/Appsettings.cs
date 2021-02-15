6420a64f713cf7a6534cdc8a9cc4b717b2e1cbb7
Rainer Nasch <rainer.nasch@microsoft.com>
2021-02-15 20:39:32 +0100

    removed obsolete settings

--- Model/Appsettings.cs
+++ Model/Appsettings.cs
@@ -2,7 +2,6 @@
 {
     public class Appsettings
     {
-        public string AIInstrumentationKey { get; set; }
         public string VSTSApiVersion { get; set; }
         public string VSTSOrganization { get; set; }
         public string PAT { get; set; }
